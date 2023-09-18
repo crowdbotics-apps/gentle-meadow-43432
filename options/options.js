ff797dd9dc2d895940bb48c15e37263c2b42ecee
Crowdbotics <team@crowdbotics.com>
2023-09-18 10:04:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gentle-meadow-43432",
+  url: "https://gentle-meadow-43432.botics.co",
+  api: "https://gentle-meadow-43432.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
